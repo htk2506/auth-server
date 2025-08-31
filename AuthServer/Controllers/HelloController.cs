d44f33087bb6770cc9bb4002c7d266ca47f73c87
Henry Kim <htk2506@gmail.com>
2025-08-30 23:08:53 -0500

    Initial project creation

---
+++ AuthServer/Controllers/HelloController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AuthServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello World");
+        }
+    }
+}
