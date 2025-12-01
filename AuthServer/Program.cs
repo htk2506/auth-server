d8f891bda2a9280e428b4ec14e77aeb82d7cb129
htk2506 <htk2506@gmail.com>
2025-11-30 21:12:05 -0600

    Add comments

--- AuthServer/Program.cs
+++ AuthServer/Program.cs
@@ -8,22 +8,28 @@ using System.Text.Json;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+// Configure database
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseNpgsql(builder.Configuration.GetConnectionString("Database")).UseSnakeCaseNamingConvention();
 });
+// Use snake case for JSON request bodies
 builder.Services.ConfigureHttpJsonOptions(options =>
 {
     options.SerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower;
     options.SerializerOptions.PropertyNameCaseInsensitive = true;
 });
+// Add PasswordHasher
 builder.Services.AddSingleton<PasswordHasher<User>>();
 var app = builder.Build();
+
 
 // Configure the app
 if (app.Environment.IsDevelopment())
 {
+    // Generate Open API spec
     app.MapOpenApi();
+    // Add Swagger UI
     app.UseSwaggerUI(options =>
     {
         options.SwaggerEndpoint("/openapi/v1.json", "v1");
@@ -32,6 +38,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
+
 
 // Run the app
 app.Run();
