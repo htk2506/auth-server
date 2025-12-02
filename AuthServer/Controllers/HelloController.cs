25ee4792c0578b65e50d37c8183c5d7a6dcbbca6
htk2506 <htk2506@gmail.com>
2025-12-01 21:50:27 -0600

    Add protected route for testing

--- AuthServer/Controllers/HelloController.cs
+++ AuthServer/Controllers/HelloController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AuthServer.Controllers
 {
@@ -8,9 +9,18 @@ namespace AuthServer.Controllers
     {
         [HttpGet("unprotected")]
         [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
-        public IActionResult UnprotectedGet()
+        public IActionResult Unprotected()
         {
             return Ok("Hello World! This route doesn't require authentication.");
+        }
+
+        [Authorize]
+        [HttpGet("protected")]
+        [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+        public IActionResult Protected()
+        {
+            // TODO: Add more specific user
+            return Ok("Hello user! This route is protected.");
         }
     }
 }
