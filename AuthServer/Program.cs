4171c25e388d6b8c0ff9a8e6863c70b5b5b53073
htk2506 <htk2506@gmail.com>
2025-12-01 20:44:21 -0600

    Update program build

--- AuthServer/Program.cs
+++ AuthServer/Program.cs
@@ -3,29 +3,41 @@ using AuthServer.Database.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 
-// Build the app
+# region Configure the builder
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddControllers();
+
+// Add API controllers
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        // Configure JSON options
+        options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower;
+        options.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    });
+
+// Add routing
 builder.Services.AddRouting(options => options.LowercaseUrls = true);
+
+// Add OpenAPI service  
 builder.Services.AddOpenApi();
+
 // Configure database
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseNpgsql(builder.Configuration.GetConnectionString("Database")).UseSnakeCaseNamingConvention();
 });
-// Use snake case for JSON request bodies
-builder.Services.ConfigureHttpJsonOptions(options =>
-{
-    options.SerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower;
-    options.SerializerOptions.PropertyNameCaseInsensitive = true;
-});
+
 // Add PasswordHasher
 builder.Services.AddSingleton<PasswordHasher<User>>();
+#endregion
+
+#region Configure the app
 var app = builder.Build();
 
-
-// Configure the app
+// For development environment
 if (app.Environment.IsDevelopment())
 {
     // Generate Open API spec
@@ -36,10 +48,16 @@ if (app.Environment.IsDevelopment())
         options.SwaggerEndpoint("/openapi/v1.json", "v1");
     });
 }
-app.UseHttpsRedirection();
-app.UseAuthorization();
-app.MapControllers();
 
+// Send HTTP requests to HTTPS
+app.UseHttpsRedirection();
+
+// Add authorization
+app.UseAuthorization();
+
+// Add endpoints for controller actions
+app.MapControllers();
+#endregion
 
 // Run the app
 app.Run();
