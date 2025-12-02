4b3099555809702e223b9c8c582f9488f68534b8
htk2506 <htk2506@gmail.com>
2025-12-01 22:18:41 -0600

    Remove unnecessary usings

--- AuthServer/Services/TokenService.cs
+++ AuthServer/Services/TokenService.cs
@@ -1,7 +1,4 @@
-﻿using AuthServer.Database;
-using AuthServer.Database.Models;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.IdentityModel.Tokens;
+﻿using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
