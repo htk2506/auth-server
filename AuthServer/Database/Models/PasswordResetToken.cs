0c2868e7fb3f266346c0b873805a25f317038f82
htk2506 <htk2506@gmail.com>
2026-01-15 22:03:45 -0600

    Add table PasswordResetTokens

---
+++ AuthServer/Database/Models/PasswordResetToken.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuthServer.Database.Models
+{
+    public class PasswordResetToken
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string TokenHash { get; set; } = null!;
+
+        [Required]
+        public AppUser AppUser { get; set; } = null!;
+
+        [Required]
+        public DateTimeOffset ExpiresAt { get; set; }
+    }
+}
