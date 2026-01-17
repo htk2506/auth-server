4dcbd2adbf5940c7dcaf733083cb412dd8a781cb
htk2506 <htk2506@gmail.com>
2026-01-17 01:03:51 -0600

    Add comments

--- AuthServer/Helpers/Utils.cs
+++ AuthServer/Helpers/Utils.cs
@@ -8,7 +8,7 @@ namespace AuthServer.Helpers
         /// Extracts a list of errors from a model state.
         /// </summary>
         /// <param name="modelState">The ModelState from a controller.</param>
-        /// <returns></returns>
+        /// <returns>List of error strings.</returns>
         public static List<string> GetModelErrors(ModelStateDictionary modelState)
         {
             var modelErrors = new List<string>();
--- AuthServer/Services/TokenService.cs
+++ AuthServer/Services/TokenService.cs
@@ -15,6 +15,10 @@ namespace AuthServer.Services
             _passwordHasher = passwordHasher;
         }
 
+        /// <summary>
+        /// Generates a token string.
+        /// </summary>
+        /// <returns>The token.</returns>
         public string GenerateToken()
         {
             using (RandomNumberGenerator rng = RandomNumberGenerator.Create())
@@ -25,11 +29,24 @@ namespace AuthServer.Services
             }
         }
 
+        /// <summary>
+        /// Hashes a token.
+        /// </summary>
+        /// <param name="user"></param>
+        /// <param name="token"></param>
+        /// <returns>The hashed token.</returns>
         public string GenerateTokenHash(AppUser user, string token)
         {
             return _passwordHasher.HashPassword(user, token);
         }
 
+        /// <summary>
+        /// Verifies if a token hash matches a token.
+        /// </summary>
+        /// <param name="user"></param>
+        /// <param name="tokenHash"></param>
+        /// <param name="token"></param>
+        /// <returns>True if the token hash matches the token and false otherwise.</returns>
         public bool VerifyHashedToken(AppUser user, string tokenHash, string token)
         {
             return _passwordHasher.VerifyHashedPassword(user, tokenHash, token) == PasswordVerificationResult.Success;
