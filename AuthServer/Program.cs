de6c8220e7881198b04bd97ac435208dd390b10c
htk2506 <htk2506@gmail.com>
2026-06-08 01:34:36 -0500

    Add Serilog

--- AuthServer/Program.cs
+++ AuthServer/Program.cs
@@ -12,183 +12,202 @@ using OpenTelemetry.Logs;
 using OpenTelemetry.Metrics;
 using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
+using Serilog;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
-# region Configure the builder
-var builder = WebApplication.CreateBuilder(args);
+// Temporary logger
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
-// Configure database
-builder.Services.AddDbContext<AppDbContext>(options =>
+try
 {
-    options.UseNpgsql(builder.Configuration.GetConnectionString("Database")).UseSnakeCaseNamingConvention();
-});
+    #region Configure the builder
+    var builder = WebApplication.CreateBuilder(args);
 
-// Configure problem details
-builder.Services.AddProblemDetails(options =>
-{
-    options.CustomizeProblemDetails = context =>
+    // Add Serilog
+    builder.Services.AddSerilog((services, loggerConfiguration) => loggerConfiguration
+        .ReadFrom.Configuration(builder.Configuration)
+        .ReadFrom.Services(services));
+
+    // Configure database
+    builder.Services.AddDbContext<AppDbContext>(options =>
     {
-        IExceptionHandlerPathFeature? exceptionHandler = context.HttpContext.Features.Get<IExceptionHandlerPathFeature>();
-        if (exceptionHandler != null)
+        options.UseNpgsql(builder.Configuration.GetConnectionString("Database")).UseSnakeCaseNamingConvention();
+    });
+
+    // Configure problem details
+    builder.Services.AddProblemDetails(options =>
+    {
+        options.CustomizeProblemDetails = context =>
         {
-            // Add info from exceptions
-            Exception error = exceptionHandler.Error;
-            context.ProblemDetails.Type = exceptionHandler.Error.GetType().Name;
-            context.ProblemDetails.Detail = exceptionHandler.Error.Message;
-        }
-    };
-});
-
-// Add routing
-builder.Services.AddRouting(options => options.LowercaseUrls = true);
-
-// Add CORS policy
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowAllOrigins", policy =>
-    {
-        policy.AllowAnyOrigin()
-              .AllowAnyHeader()
-              .AllowAnyMethod();
-    });
-});
-
-// Add authentication
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = "SessionJwtScheme";
-    options.DefaultChallengeScheme = "SessionJwtScheme";
-})
-    .AddScheme<AuthenticationSchemeOptions, SessionJwtAuthenticationHandler>("SessionJwtScheme", null);
-
-// Configure API endpoint versioning
-builder.Services.AddApiVersioning(options =>
-{
-    options.DefaultApiVersion = new ApiVersion(1);
-    options.AssumeDefaultVersionWhenUnspecified = true;
-    options.ReportApiVersions = true;
-    options.ApiVersionReader = new UrlSegmentApiVersionReader();
-})
-    .AddApiExplorer(options =>
-    {
-        options.GroupNameFormat = "'v'VVV";
-        options.SubstituteApiVersionInUrl = true;
+            IExceptionHandlerPathFeature? exceptionHandler = context.HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+            if (exceptionHandler != null)
+            {
+                // Add info from exceptions
+                Exception error = exceptionHandler.Error;
+                context.ProblemDetails.Type = exceptionHandler.Error.GetType().Name;
+                context.ProblemDetails.Detail = exceptionHandler.Error.Message;
+            }
+        };
     });
 
-// Add Swagger doc generation
-builder.Services.AddSwaggerGen(options =>
-{
-    options.SwaggerDoc("v1", new OpenApiInfo { Title = "Auth Server", Version = "v1" });
-    options.SwaggerDoc("v2", new OpenApiInfo { Title = "Auth Server", Version = "v2" });
+    // Add routing
+    builder.Services.AddRouting(options => options.LowercaseUrls = true);
 
-    // Define Bearer token authentication schema
-    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    // Add CORS policy
+    builder.Services.AddCors(options =>
     {
-        In = ParameterLocation.Header,
-        Description = "Session token",
-        Name = "Authorization",
-        Type = SecuritySchemeType.Http,
-        Scheme = "bearer"
+        options.AddPolicy("AllowAllOrigins", policy =>
+        {
+            policy.AllowAnyOrigin()
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
     });
 
-    // Use Bearer schema
-    options.AddSecurityRequirement(document => new OpenApiSecurityRequirement
+    // Add authentication
+    builder.Services.AddAuthentication(options =>
     {
+        options.DefaultAuthenticateScheme = "SessionJwtScheme";
+        options.DefaultChallengeScheme = "SessionJwtScheme";
+    })
+        .AddScheme<AuthenticationSchemeOptions, SessionJwtAuthenticationHandler>("SessionJwtScheme", null);
+
+    // Configure API endpoint versioning
+    builder.Services.AddApiVersioning(options =>
+    {
+        options.DefaultApiVersion = new ApiVersion(1);
+        options.AssumeDefaultVersionWhenUnspecified = true;
+        options.ReportApiVersions = true;
+        options.ApiVersionReader = new UrlSegmentApiVersionReader();
+    })
+        .AddApiExplorer(options =>
+        {
+            options.GroupNameFormat = "'v'VVV";
+            options.SubstituteApiVersionInUrl = true;
+        });
+
+    // Add Swagger doc generation
+    builder.Services.AddSwaggerGen(options =>
+    {
+        options.SwaggerDoc("v1", new OpenApiInfo { Title = "Auth Server", Version = "v1" });
+        options.SwaggerDoc("v2", new OpenApiInfo { Title = "Auth Server", Version = "v2" });
+
+        // Define Bearer token authentication schema
+        options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+        {
+            In = ParameterLocation.Header,
+            Description = "Session token",
+            Name = "Authorization",
+            Type = SecuritySchemeType.Http,
+            Scheme = "bearer"
+        });
+
+        // Use Bearer schema
+        options.AddSecurityRequirement(document => new OpenApiSecurityRequirement
+        {
         {
             new OpenApiSecuritySchemeReference("Bearer", document),
             new List<string>()
         }
+        });
     });
-});
 
-// Add health check
-builder.Services.AddHealthChecks();
+    // Add health check
+    builder.Services.AddHealthChecks();
 
-// Add OpenTelemetry
-string serviceName = builder.Configuration.GetValue<string>("ServiceName") ?? throw new NullReferenceException("Missing ServiceName.");
-string serviceVersion = builder.Configuration.GetValue<string>("ServiceVersion") ?? throw new NullReferenceException("Missing ServiceVersion.");
-builder.Services.AddOpenTelemetry()
-    .ConfigureResource(resource => resource.AddService(
-        serviceName: serviceName,
-        serviceVersion: serviceVersion))
-    .WithTracing(tracing => tracing
-        .AddSource(serviceName)
-        .AddAspNetCoreInstrumentation()
-        .AddHttpClientInstrumentation()
-        .AddConsoleExporter())
-    .WithMetrics(metrics => metrics
-        .AddMeter(serviceName)
-        .AddConsoleExporter());
+    // Add OpenTelemetry
+    string serviceName = builder.Configuration.GetValue<string>("ServiceName") ?? throw new NullReferenceException("Missing ServiceName.");
+    string serviceVersion = builder.Configuration.GetValue<string>("ServiceVersion") ?? throw new NullReferenceException("Missing ServiceVersion.");
+    builder.Services.AddOpenTelemetry()
+        .ConfigureResource(resource => resource.AddService(
+            serviceName: serviceName,
+            serviceVersion: serviceVersion))
+        .WithTracing(tracing => tracing
+            .AddSource(serviceName)
+            .AddAspNetCoreInstrumentation()
+            .AddHttpClientInstrumentation()
+            .AddConsoleExporter())
+        .WithMetrics(metrics => metrics
+            .AddMeter(serviceName)
+            .AddConsoleExporter());
 
-builder.Logging.AddOpenTelemetry(options => options
-    .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(
-        serviceName: serviceName,
-        serviceVersion: serviceVersion))
-    .AddConsoleExporter());
+    // Add API controllers
+    builder.Services.AddControllers()
+        .AddJsonOptions(options =>
+        {
+            // Configure JSON options
+            options.JsonSerializerOptions.WriteIndented = true;
+            options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower;
+            options.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
+            options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+        });
 
-// Add API controllers
-builder.Services.AddControllers()
-    .AddJsonOptions(options =>
+    // Add services
+    builder.Services.AddSingleton<PasswordHasher<AppUser>>();
+    builder.Services.AddSingleton<JwtService>();
+    builder.Services.AddSingleton<TokenService>();
+    builder.Services.AddScoped<EmailService>();
+    #endregion
+
+    #region Configure the app
+    var app = builder.Build();
+
+    // Add Trace-ID headers
+    app.UseMiddleware<TraceIdMiddleware>();
+
+    // Streamline request logging
+    app.UseSerilogRequestLogging();
+
+    // Catch exceptions
+    app.UseExceptionHandler();
+
+    // Use HTTPS redirection if there's an HTTPS URL
+    string urls = builder.WebHost.GetSetting(WebHostDefaults.ServerUrlsKey) ?? "";
+    if (urls.ToLower().Contains("https"))
     {
-        // Configure JSON options
-        options.JsonSerializerOptions.WriteIndented = true;
-        options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower;
-        options.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
-        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+        app.UseHttpsRedirection();
+    }
+
+    // Enable problem details to be returned when error response is otherwise empty
+    app.UseStatusCodePages();
+
+    // Route to correct endpoint
+    app.UseRouting();
+
+    // Use CORS policy
+    app.UseCors("AllowAllOrigins");
+
+    // Use authentication and authorization
+    app.UseAuthentication();
+    app.UseAuthorization();
+
+    // Use Swagger UI
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.RoutePrefix = string.Empty;
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.SwaggerEndpoint("/swagger/v2/swagger.json", "v2");
     });
 
-// Add services
-builder.Services.AddSingleton<PasswordHasher<AppUser>>();
-builder.Services.AddSingleton<JwtService>();
-builder.Services.AddSingleton<TokenService>();
-builder.Services.AddScoped<EmailService>();
-#endregion
+    // Map the health check endpoint
+    app.MapHealthChecks("/healthz");
 
-#region Configure the app
-var app = builder.Build();
+    // Map endpoints for controller actions
+    app.MapControllers();
+    #endregion
 
-// Add Trace-ID headers
-app.UseMiddleware<TraceIdMiddleware>();
-
-// Catch exceptions
-app.UseExceptionHandler();
-
-// Use HTTPS redirection if there's an HTTPS URL
-string urls = builder.WebHost.GetSetting(WebHostDefaults.ServerUrlsKey) ?? "";
-if (urls.ToLower().Contains("https"))
-{
-    app.UseHttpsRedirection();
+    // Run the app
+    app.Run();
 }
-
-// Enable problem details to be returned when error response is otherwise empty
-app.UseStatusCodePages();
-
-// Route to correct endpoint
-app.UseRouting();
-
-// Use CORS policy
-app.UseCors("AllowAllOrigins");
-
-// Use authentication and authorization
-app.UseAuthentication();
-app.UseAuthorization();
-
-// Use Swagger UI
-app.UseSwagger();
-app.UseSwaggerUI(options =>
+catch (Exception ex)
 {
-    options.RoutePrefix = string.Empty;
-    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-    options.SwaggerEndpoint("/swagger/v2/swagger.json", "v2");
-});
-
-// Map the health check endpoint
-app.MapHealthChecks("/healthz");
-
-// Map endpoints for controller actions
-app.MapControllers();
-#endregion
-
-// Run the app
-app.Run();
+    Log.Fatal(ex, "Server terminated unexpectedly.");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
